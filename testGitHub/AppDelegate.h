46b4fb866bb8f4c1bdf24537c06730a6579dca74
mr-crockpot <adamschor@adams-macbook-air.local>
2018-06-02 22:21:13 -0600

    Initial Commit

---
+++ testGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGitHub
+//
+//  Created by Adam Schor on 6/2/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGitHub
+//
+//  Created by Adam Schor on 6/2/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
